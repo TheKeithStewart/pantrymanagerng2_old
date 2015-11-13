01ab49004019a10d6280613c656b1ae3f866c1aa
Keith Stewart <kgstewart@gmail.com>
2015-11-13 09:37:55 -0600

    rearrange serve files to allow for all request paths to serve the single index.html file

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
-/// <reference path="./typings/tsd.d.ts" />
+/// <reference path="./server/typings/tsd.d.ts" />
 
 var Router = require('falcor-router');
 
 import express = require('express');
 var app = express();
 
-var datamodel = require(__dirname + '/datamodel.js');
+var datamodel = require(__dirname + '/server/datamodel.js');
 app.use('/model.json', datamodel);
 
 // serve static files from current directory
 app.use(express.static('./src/client'));
-app.use(express.static('./'))
+app.use(express.static('./'));
+process.chdir('./');
+app.get('*', function(req, res) {
+	res.sendFile(__dirname + '/client/index.html')
+});
 
 var server = app.listen(3000);
