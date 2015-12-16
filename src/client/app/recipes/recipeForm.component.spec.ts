6c97aca2e56d7d1d9d7d07bd5be4d016e2e9e34a
Keith Stewart <kgstewart@gmail.com>
2015-12-16 03:43:53 -0600

    add spec

---
+++ src/client/app/recipes/recipeForm.component.spec.ts
@@ -0,0 +1,26 @@
+import {
+	iit,
+	it,
+	ddescribe,
+	describe,
+	expect,
+	inject,
+	injectAsync,
+	TestComponentBuilder,
+	beforeEachProviders,
+	fakeAsync,
+	tick
+} from 'angular2/testing';
+import {Component, provide} from 'angular2/core';
+
+import {RecipeFormComponent} from './recipeForm.component';
+
+describe('Component: recipeForm', () => {
+  
+  beforeEachProviders(() => []);
+
+  xit('should ...', () => {
+	  
+  });
+
+});
